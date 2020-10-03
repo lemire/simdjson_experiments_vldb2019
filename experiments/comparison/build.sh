c55ae434879001701962539631f40a60ddd30cda
Paul Dreik <github@pauldreik.se>
2020-10-03 19:25:49 +0200

    bump to cmake build instead of makefile

--- experiments/comparison/build.sh
+++ experiments/comparison/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -eux
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH/../../library/simdjson
-make -k parsingcompetition  allparsingcompetition 
+mkdir build-benchmark
+cd build-benchmark
+cmake .. -DCMAKE_BUILD_TYPE=Release
+# older cmake (3.13) needs one target at a time
+cmake --build . -j $(nproc) --target parsingcompetition
+cmake --build . -j $(nproc) --target allparsingcompetition
+
+echo "$0: done building"
+
